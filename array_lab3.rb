5ac5bc17520705c5f9146a29b4915f2dd1c4d5de
fishermanswharff <fishermanswharff@github.com>
2014-09-24 15:05:57 -0400

    removing code that didn't execute

--- array_lab3.rb
+++ array_lab3.rb
@@ -1,5 +1,5 @@
 name = "sean ian michael segfault bartholomew joke-explainer jones"
 array = name.split(" ")
-array.each { |item| item == "bartholomew" ? array.delete(item) : item.to_s.upcase }
+array.each { |item| item == "bartholomew" ? array.delete(item) : item }
 array.map! { |item| item.to_s.capitalize }
 print array.join(" ")
