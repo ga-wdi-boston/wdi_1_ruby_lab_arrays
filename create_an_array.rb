efd44f0dc514cede1138ddf5172b7f8862ecba89
Jonathan Symond <jsyzer@Jonathans-MacBook-Air.local>
2014-09-24 15:09:53 -0400

    second repo

---
+++ create_an_array.rb
@@ -0,0 +1,28 @@
+require "pry"
+
+# Problem #1
+days_of_the_week = ["Monday", "Tuesday", "Wednesday" "Thursday", "Friday", "Saturday", "Sunday"]
+
+# Problem #2
+days_of_the_week.unshift("Sunday").pop
+puts day_of_the_week
+
+# Problem # 3
+put days_of_the_week[3].upcase
+
+
+# Problem # 4 + 5
+array = [["Monday", "Tuesday", "Wednesday", "Thursday" "Friday"],
+[Saturday, Sunday]]
+array[0][2] = "Woden's Day"
+
+# problem # 6
+array.delete_at[0]
+
+# Problem # 7
+days_week = days_of_the_week.sort!
+
+# Problem #8
+days_week.each do |days|
+  puts days
+end
