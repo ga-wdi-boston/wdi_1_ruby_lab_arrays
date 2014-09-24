cd1a2a50fd5f421cb3f81afd90f37152b8c37e7f
fishermanswharff <fishermanswharff@github.com>
2014-09-24 14:54:38 -0400

    cleaning up array_lab3.rb

--- array_lab3.rb
+++ array_lab3.rb
@@ -1,5 +1,4 @@
 name = "sean ian michael segfault bartholomew joke-explainer jones"
-
 array = name.split(" ")
 array.each { |item| item == "bartholomew" ? array.delete(item) : item.to_s.upcase }
 array.map! { |item| item.to_s.capitalize }
