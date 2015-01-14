a4adc14b0eb5cb87ca5eee1f276af136ed16e36a
Doug <djmacdonald@uchicago.edu>
2015-01-13 21:21:09 -0500

    Solutions to 6 exercises.

--- lib/names.rb
+++ lib/names.rb
@@ -1,2 +1,10 @@
 def name_formatter(name)
+  array = name.split(' ')
+  runner = ""
+  array.map do |word|
+    if word != "bartholomew"
+      runner = runner + " " + word.capitalize
+    end
+  end
+  return runner.strip
 end
--- lib/only_primes.rb
+++ lib/only_primes.rb
@@ -1,2 +1,10 @@
+require 'prime.rb'
 def only_primes(array)
+  new_array = []
+  array.each do |x|
+    if x.prime?
+      new_array << x
+    end
+  end
+  return new_array
 end
