e1fef1d1846f1eb8665b77fe3fd554e64d9bc50d
Caitlyn Landry <caitlyn.landry@gmail.com>
2015-01-13 23:07:04 -0500

    Add Ruby Array Lab solutions

--- lib/cube_numbers.rb
+++ lib/cube_numbers.rb
@@ -1,2 +1,5 @@
 def cube_numbers(numbers)
+  numbers.map do |number|
+    number ** 3
+  end
 end
--- lib/only_primes.rb
+++ lib/only_primes.rb
@@ -1,2 +1,9 @@
+require 'prime'
+
 def only_primes(array)
+  array.each do |n|
+      if n.prime? == false
+        array.delete(n)
+      end
+  end
 end
