5e91997b51cd931da8d72a855453c518b1c879f2
Ross <rdegnen@gmail.com>
2015-01-13 21:49:00 -0500

    All of the methods passed except the last one 'only_squares_can_join'

--- lib/only_primes.rb
+++ lib/only_primes.rb
@@ -1,2 +1,10 @@
 def only_primes(array)
+  array.map do |int|
+    if int == 1
+      array.delete(int)
+    elsif int % 2 == 0
+      array.delete(int)
+    end
+  end
+  array
 end
--- lib/only_squares_can_join.rb
+++ lib/only_squares_can_join.rb
@@ -1,2 +1,10 @@
-def only_squares_can_join
+def only_squares_can_join(array, *num)
+  join = num.join.to_i
+  numbers = join.split
+
+  sqrt = numbers ** 0.5
+
+  if numbers ** 0.5 == sqrt
+    array << numbers
+  end
 end
