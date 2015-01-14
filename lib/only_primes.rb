43b2427724963fbe8eceb797a8295588670d3ca0
Helen Thi <ht5658@gmail.com>
2015-01-13 19:56:23 -0500

    ruby lab arrays

--- lib/only_primes.rb
+++ lib/only_primes.rb
@@ -1,2 +1,16 @@
 def only_primes(array)
+  array.each do |number|
+    if number%2 == 0 && number !=2
+      array.delete(number)
+    end
+    if number%3 == 0 && number !=3
+        array.delete(number)
+    end
+    if number == 1
+      array.delete(number)
+    end
+  end
+  return array
 end
+
+
--- lib/only_squares_can_join.rb
+++ lib/only_squares_can_join.rb
@@ -1,2 +1,19 @@
-def only_squares_can_join
+def only_squares_can_join (array, *numbers)
+  new_array = []
+  array.each do |x|
+    number = Math.sqrt(x)
+    number_2 = number ** 2
+    if x == number_2
+      new_array.push(x)
+    end
+  end
+  numbers.each do |y|
+    num = Math.sqrt(y)
+    num_2 = num ** 2
+    if y == num_2
+      new_array.push(y)
+    end
+  end
+  return new_array
 end
+
