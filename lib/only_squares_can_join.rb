ab3c1351c067987fbd771a4f4e81bd3a52f9a037
Nicole Pupillo <nicole.pupillo@gmail.com>
2015-01-13 23:32:57 -0500

    Completed name & only_primes exercises. only_squares_can_join still in progress.

--- lib/only_squares_can_join.rb
+++ lib/only_squares_can_join.rb
@@ -1,2 +1,8 @@
+#Objective:
+# The method only_squares_can_join takes an array of numbers, and one or more Fixnums.
+# Returns array of original array and new elements, but only if the new numbers are square'
+# For ex, expect(only_squares_can_join([4, 16], 9)).to eq ([4, 16, 9])
+
 def only_squares_can_join
+
 end
