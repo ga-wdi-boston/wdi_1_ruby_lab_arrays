b1599d4ba1731a9617db53395682e57c7696ac5f
J.Marx <jennyrutledge@gmail.com>
2015-01-13 21:53:05 -0500

    Create only_squares_can_join.rb

--- lib/only_squares_can_join.rb
+++ lib/only_squares_can_join.rb
@@ -1,2 +1,19 @@
-def only_squares_can_join
+
+
+
+def only_squares_can_join (inArr, *args)
+  inNums = args
+
+  inNums.each do |num|
+    temp = num ** 0.5
+    double_temp = temp ** 2
+
+    if double_temp == num
+      inArr << num
+    end
+
+  end
+
+  inArr
+
 end
