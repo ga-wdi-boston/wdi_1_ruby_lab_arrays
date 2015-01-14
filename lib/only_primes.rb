a91087402b05d5f492415a7244565d5471b0a2d6
idelairre <idelairre@gmail.com>
2015-01-13 19:59:41 -0500

    modified methods to pass rspec tests, includes variations on other peoples solutions

--- lib/only_primes.rb
+++ lib/only_primes.rb
@@ -1,2 +1,12 @@
+#get an array of numbers, return the same array but with all the non-prime numbers removed.
+require 'prime.rb'
+
 def only_primes(array)
+  prime_array = []
+  array.each do |n|
+    if n.prime? == true
+      prime_array << n
+    end
+  end
+  return prime_array
 end
