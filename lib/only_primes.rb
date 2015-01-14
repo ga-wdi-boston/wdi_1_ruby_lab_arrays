9643816b83f9f0344ca8122a96332c453a524afa
J.Marx <jennyrutledge@gmail.com>
2015-01-13 21:54:13 -0500

    Create only_primes.rb

--- lib/only_primes.rb
+++ lib/only_primes.rb
@@ -1,2 +1,31 @@
 def only_primes(array)
+  outArr = []
+  array.each do |elem|
+    temp = isPrime?(elem)
+    if temp
+      outArr << temp
+    end
+  end
+  outArr
+end
+
+
+def isPrime? (num)
+
+  flag = true
+  count = 2
+  root = Math.sqrt(num)
+  while count <= root && flag == true
+    if num % count == 0
+      flag = false
+    end
+  count += 1
+  end
+
+  if flag == true && num > 1
+    num
+  else
+    false
+  end
+
 end
